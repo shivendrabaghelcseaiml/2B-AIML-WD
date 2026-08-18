6d8dee493b64755d60d209a9bf3a1ce18fc73dcb
Shivendra Singh Baghel <ssinghbaghel59@gmail.com>
2026-08-18 12:12:33 +0530

    add

---
+++ mathsfunction.js
@@ -0,0 +1,16 @@
+let n = 25.7;
+
+console.log("Square Root:", Math.sqrt(n));
+console.log("Power:", Math.pow(5, 2));
+console.log("Absolute:", Math.abs(-25));
+console.log("Round:", Math.round(n));
+console.log("Floor:", Math.floor(n));
+console.log("Ceil:", Math.ceil(n));
+console.log("Maximum:", Math.max(10, 20, 30, 40));
+console.log("Minimum:", Math.min(10, 20, 30, 40));
+console.log("Random:", Math.random());
+console.log("Sign:", Math.sign(-25));
+console.log("Cube Root:", Math.cbrt(27));
+console.log("Trunc:", Math.trunc(25.7));
+console.log("PI:", Math.PI);
+console.log("Euler's Number:", Math.E);
