e86aba841c08f2ff12481af7237485351a15bba5
Shivendra Singh Baghel <ssinghbaghel59@gmail.com>
2026-08-27 11:20:39 +0530

    first

--- object1.js
+++ object1.js
@@ -1,9 +1,16 @@
-let student = new Object();
-    student.name = "shivendra";
-    student.lastName = "Singh";
-    student.city = "kanpur nagar";
-    student.pincode = 209401;
-    student.salary = function() {
-    console.log("Salary is 50000");
+let student = {
+    name: "Shivendra singh",
+    lastName: "baghel",
+    city:"ghaziabad",
+    pincode:209401,
+    salary:function(){
+        console.log("Salary is 50000");
+    },
+    subjects:["Maths","Java","C++"]
 };
-    student.subjects = ["Maths", "Java", "C++"];
+console.log(student.name);
+console.log(student.lastName);
+console.log(student.city);
+console.log(student.pincode);
+console.log(student.subjects);
+student.salary();
