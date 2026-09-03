f534ef05b5c2f986593c1a43a26354a1f82e9188
Shivendra Singh Baghel <ssinghbaghel59@gmail.com>
2026-09-03 11:39:31 +0530

    arrow fuction

--- arrowfunction.js
+++ arrowfunction.js
@@ -9,3 +9,29 @@ Hello();
 
 Hello = () => console.log("hellow world");
 Hello();
+
+// pattern using arrow fucnction 
+
+// pattern = (n)=>{
+//     for (let i=0 ;i<n ; i++){
+//         for (let j=0; j<=i;j++){
+//             row +="*";
+//         }
+//         console.log("*");
+//     }
+// }
+// pattern(3);
+
+const pattern = (n) => {
+    for (let i = 0; i < n; i++) {
+        let row = "";
+        
+        for (let j = 0; j <= i; j++) {
+            row += "*";
+        }
+        
+        console.log(row);
+    }
+};
+
+pattern(3);
