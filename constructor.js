00084e84c95e7bfe84604331089bb2ee57e359cf
Shivendra Singh Baghel <ssinghbaghel59@gmail.com>
2026-09-15 11:34:50 +0530

    con

--- constructor.js
+++ constructor.js
@@ -1,7 +1,17 @@
-class Hello {
-    constructor() {
-        console.log("hello constructor");
+class student {
+    constructor(roll, name, Year)
+    {
+        this.RollNumber=roll;
+        this.Name=name;
+        this.Year=Year;
+
+
+        console.log(this.RollNumber);
+        console.log(this.Name);
+        console.log(this.Year);
     }
 }
 
-let h = new Hello();
+let s1 = new student ();
+let s2 = new student (101,"shivendra","2nd");
+let s3 = new student (202,"singh","4th");
